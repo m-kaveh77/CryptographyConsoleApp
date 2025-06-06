a02e4648a452c75e3392569fd806f2f1601924ac
Mohsen Kaveh <mohsenkaveh18@gmail.com>
2025-06-06 19:21:26 +0330

    The project initialized.

--- CryptographyConsoleApp/Program.cs
+++ CryptographyConsoleApp/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Cryptography Console App");
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine("-----------------------------------------------------------");
+    Console.WriteLine("Welcome to Cryptography Console App");
+    Console.WriteLine("-----------------------------------------------------------");
+
+    Console.WriteLine();
+
+    Console.WriteLine("------------------- Application options -------------------");
+    Console.WriteLine();
+    Console.WriteLine("1. Reverse string");
+    Console.WriteLine("2. Hash functions");
+    Console.WriteLine("3. Symmertic algorithms");
+    Console.WriteLine("4. Asymmetric algorithms");
+    Console.WriteLine("5. Exit");
+    Console.WriteLine();
+    Console.WriteLine("-----------------------------------------------------------");
+
+    Console.Write("Please select your option: ");
+
+    var option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+        default:
+            break;
+    }
+}
